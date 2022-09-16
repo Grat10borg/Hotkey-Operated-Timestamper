c4b14d52775d7c1d21426e5aeb12710f478c25d3
Grat <109081987+Grat10borg@users.noreply.github.com>
2022-09-16 09:03:48 +0200

    Updated Docs to link back to H.O.T Wiki

--- Docs.php
+++ Docs.php
@@ -9,6 +9,28 @@ include "includes/html/HtmlDoc.php";
         </div>
         <div class="m-5 p-5 mt-0">
             <h2>Docs</h2>
+            <a class="mb-5" href="https://github.com/Grat10borg/Hotkey-Operated-Timestamper/wiki">This is a Copy of the Wiki for H.O.T Found here</a>
+            <p class="mt-2">H.O.T: Hotkey Operated Timestamper & Highlight Oriented Timestamper were made to make Timestamping VODs from Streams Easier</p>
+            <p>i hope you'll find H.O.T as helpful as i find it!</p>
+            <p>please pick any point you want to read about</p>
+            <p class="mt-5">• Setting Up H.O.T for Local Timestamps</p>
+            <ul>
+                <li><a href="https://github.com/Grat10borg/Hotkey-Operated-Timestamper/wiki/Installing-InfoWriter-Plugin-for-OBS">Installing InfoWriter Plugin for OBS</a></li>
+                <li><a href="https://github.com/Grat10borg/Hotkey-Operated-Timestamper/wiki/Setting-Up-Xampp-for-running-H.O.T">Setting Up Xampp for running H.O.T</a></li>
+            </ul>
+            <p class="mt-5">• Setting Up H.O.T for Local Timestamps and Clip-Stamps</p>
+            <ul>
+                <li><a href="https://github.com/Grat10borg/Hotkey-Operated-Timestamper/wiki/Installing-InfoWriter-Plugin-for-OBS">Installing InfoWriter Plugin for OBS</a></li>
+                <li><a href="https://github.com/Grat10borg/Hotkey-Operated-Timestamper/wiki/Setting-Up-Xampp-for-running-H.O.T">Setting Up Xampp for running H.O.T</a></li>
+                <li><a href="https://github.com/Grat10borg/Hotkey-Operated-Timestamper/wiki/Making-a-Twitch-App-Access-Token">Making a Twitch App Access Token</a></li>
+                <li><a href="https://github.com/Grat10borg/Hotkey-Operated-Timestamper/wiki/Making-a-Youtube-Client-Id-and-Api-Key">Making a Youtube Client Id and Api Key</a></li>
+                <li><a href="https://github.com/Grat10borg/Hotkey-Operated-Timestamper/wiki/Making-Your-Settings-for-H.O.T">Making Your Settings for H.O.T</a></li>
+            </ul>
+            <p class="mt-5">• Using H.O.T</p>
+            <ul>
+                <li><a href="https://github.com/Grat10borg/Hotkey-Operated-Timestamper/wiki/Using-H.O.T:-Description-Maker">Using H.O.T: Description Maker : Hotkey Operated Timestamper</a></li>
+                <li><a href="https://github.com/Grat10borg/Hotkey-Operated-Timestamper/wiki/Using-H.O.T-Clip-Highlight-Maker">Using H.O.T Clip Highlight Maker : Highlight Oriented Timestamper</a></li>
+            </ul>
         </div>
     </main>
 </body>
